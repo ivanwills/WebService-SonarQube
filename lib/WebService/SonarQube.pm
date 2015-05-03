d91a269263dbdac4e997a9033ac7c3e7a2c4b10d
Ivan Wills <ivan.wills@gmail.com>
2015-05-03 12:59:40 +1000

    Refactoring getting data so that the correct types of lookups can be done (ie get/post)

--- lib/WebService/SonarQube.pm
+++ lib/WebService/SonarQube.pm
@@ -9,7 +9,6 @@ package WebService::SonarQube;
 use Moo;
 use strict;
 use warnings;
-use version;
 use Carp;
 use namespace::clean;
 use English qw/ -no_match_vars /;
@@ -19,6 +18,7 @@ use Types::Standard -types;
 use URI;
 use WWW::Mechanize;
 use JSON;
+use Try::Tiny;
 
 our $VERSION = 0.01;
 
@@ -42,6 +42,9 @@ has version => (
     is  => 'rw',
     isa => Str,
 );
+has commands => (
+    is => 'rw',
+);
 
 sub BUILD {
     my ($self) = @_;
@@ -54,8 +57,35 @@ sub BUILD {
         $self->url($url);
     }
 
-    my $data = $self->server_index();
-    $self->version($data->{version});
+    $self->_get_commands();
+
+    my $server = $self->server_index();
+    $self->version($server->{version});
+}
+
+sub _get_commands {
+    my ($self) = @_;
+
+    my $list = $self->_get('webservices/list',  include_internals => 'true');
+    use Data::Dumper qw/Dumper/;
+    warn Dumper $list->{webServices}[0];
+
+    my %commands;
+    for my $ws (@{ $list->{webServices}}) {
+        my $name = $ws->{path};
+        $name =~ s{^api/}{};
+
+        for my $action (@{ $ws->{actions} }) {
+            $commands{$name . '_' . $action->{key}} = {
+                name     => $name . '_' . $action->{key},
+                url      => $name . '/' . $action->{key},
+                internal => !!$action->{internal},
+                post     => !!$action->{post},
+            };
+        }
+    }
+
+    die Dumper \%commands
 }
 
 our $AUTOLOAD;
@@ -66,16 +96,24 @@ sub AUTOLOAD {
     $api =~ s{.*::}{};
     $api =~ s{_}{/}g;
 
-    my $uri = URI->new($self->url . '/api/' . $api);
-    $uri->query_form(
-        map {
-            s/_(.)/uc $1/egxms;
-            ($_ => $params{$_});
-        }
-        keys %params
-    );
+    my $result;
+    try {
+        $result = $self->_get($api, %params);
+    }
+    catch {
+        confess "Errored trying $AUTOLOAD()\n$_\n";
+    }
+
+    return $result;
+}
+
+sub _get {
+    my ($self, $api, %params) = @_;
 
     my $mech = $self->mech;
+    my $uri = URI->new($self->url . '/api/' . $api);
+    $uri->query_form(%params);
+
     $mech->get($uri);
 
     return decode_json($mech->content || '{}');
@@ -159,112 +197,112 @@ Simple interface to accessing SonarQube's web API.
 
 =head1 SUBROUTINES/METHODS
 
-POST api/action_plans/close 
-POST api/action_plans/create 
-POST api/action_plans/delete 
-POST api/action_plans/open 
+POST api/action_plans/close
+POST api/action_plans/create
+POST api/action_plans/delete
+POST api/action_plans/open
  GET api/action_plans/search
-POST api/action_plans/update 
- GET api/authentication/validate 
- GET api/coverage/show 
- GET api/duplications/show 
- GET api/events/index 
- GET api/favorites/index 
- GET api/issue_filters/favorites 
- GET api/issue_filters/show 
-POST api/issues/add_comment 
-POST api/issues/assign 
- GET api/issues/authors 
-POST api/issues/bulk_change 
- GET api/issues/changelog 
-POST api/issues/create 
-POST api/issues/delete_comment 
-POST api/issues/do_action 
-POST api/issues/do_transition 
-POST api/issues/edit_comment 
-POST api/issues/plan 
- GET api/issues/search 
-POST api/issues/set_severity 
- GET api/languages/list 
- GET api/manual_measures/index 
+POST api/action_plans/update
+ GET api/authentication/validate
+ GET api/coverage/show
+ GET api/duplications/show
+ GET api/events/index
+ GET api/favorites/index
+ GET api/issue_filters/favorites
+ GET api/issue_filters/show
+POST api/issues/add_comment
+POST api/issues/assign
+ GET api/issues/authors
+POST api/issues/bulk_change
+ GET api/issues/changelog
+POST api/issues/create
+POST api/issues/delete_comment
+POST api/issues/do_action
+POST api/issues/do_transition
+POST api/issues/edit_comment
+POST api/issues/plan
+ GET api/issues/search
+POST api/issues/set_severity
+ GET api/languages/list
+ GET api/manual_measures/index
  GET api/metrics/index
-POST api/permissions/add 
-POST api/permissions/remove 
-POST api/profiles/backup 
-POST api/profiles/destroy 
- GET api/profiles/index 
- GET api/profiles/list 
-POST api/profiles/restore 
-POST api/profiles/set_as_default 
-POST api/projects/create 
-POST api/projects/destroy 
- GET api/projects/index 
- GET api/properties/index 
-POST api/qualitygates/copy 
-POST api/qualitygates/create 
-POST api/qualitygates/create_condition 
-POST api/qualitygates/delete_condition 
-POST api/qualitygates/deselect 
-POST api/qualitygates/destroy 
- GET api/qualitygates/list 
-POST api/qualitygates/rename 
- GET api/qualitygates/search 
-POST api/qualitygates/select 
-POST api/qualitygates/set_as_default 
- GET api/qualitygates/show 
+POST api/permissions/add
+POST api/permissions/remove
+POST api/profiles/backup
+POST api/profiles/destroy
+ GET api/profiles/index
+ GET api/profiles/list
+POST api/profiles/restore
+POST api/profiles/set_as_default
+POST api/projects/create
+POST api/projects/destroy
+ GET api/projects/index
+ GET api/properties/index
+POST api/qualitygates/copy
+POST api/qualitygates/create
+POST api/qualitygates/create_condition
+POST api/qualitygates/delete_condition
+POST api/qualitygates/deselect
+POST api/qualitygates/destroy
+ GET api/qualitygates/list
+POST api/qualitygates/rename
+ GET api/qualitygates/search
+POST api/qualitygates/select
+POST api/qualitygates/set_as_default
+ GET api/qualitygates/show
 POST api/qualitygates/unset_default
-POST api/qualitygates/update_condition 
-POST api/qualityprofiles/activate_rule 
-POST api/qualityprofiles/activate_rules 
-POST api/qualityprofiles/deactivate_rule 
-POST api/qualityprofiles/deactivate_rules 
-POST api/qualityprofiles/restore_built_in 
- GET api/resources/index 
-POST api/rules/create 
-POST api/rules/delete 
- GET api/rules/repositories 
- GET api/rules/search 
- GET api/rules/show 
- GET api/rules/tags 
-POST api/rules/update 
- GET api/server/index 
-POST api/server/setup 
- GET api/sources/raw 
- GET api/sources/scm 
- GET api/sources/show 
- GET api/system/info 
-POST api/system/restart 
- GET api/tests/covered_files 
- GET api/tests/show 
- GET api/tests/test_cases 
- GET api/timemachine/index 
- GET api/updatecenter/installed_plugins 
- GET api/user_properties/index 
-POST api/users/create 
-POST api/users/deactivate 
- GET api/users/search 
-POST api/users/update 
-POST api/views/add_local_view 
-POST api/views/add_project 
-POST api/views/add_remote_view 
+POST api/qualitygates/update_condition
+POST api/qualityprofiles/activate_rule
+POST api/qualityprofiles/activate_rules
+POST api/qualityprofiles/deactivate_rule
+POST api/qualityprofiles/deactivate_rules
+POST api/qualityprofiles/restore_built_in
+ GET api/resources/index
+POST api/rules/create
+POST api/rules/delete
+ GET api/rules/repositories
+ GET api/rules/search
+ GET api/rules/show
+ GET api/rules/tags
+POST api/rules/update
+ GET api/server/index
+POST api/server/setup
+ GET api/sources/raw
+ GET api/sources/scm
+ GET api/sources/show
+ GET api/system/info
+POST api/system/restart
+ GET api/tests/covered_files
+ GET api/tests/show
+ GET api/tests/test_cases
+ GET api/timemachine/index
+ GET api/updatecenter/installed_plugins
+ GET api/user_properties/index
+POST api/users/create
+POST api/users/deactivate
+ GET api/users/search
+POST api/users/update
+POST api/views/add_local_view
+POST api/views/add_project
+POST api/views/add_remote_view
 POST api/views/add_sub_view
-POST api/views/create 
- GET api/views/define 
-POST api/views/delete 
- GET api/views/list 
- GET api/views/local_views 
-POST api/views/manual_measure 
-POST api/views/mode 
-POST api/views/move 
- GET api/views/move_options 
- GET api/views/projects 
-POST api/views/regexp 
- GET api/views/remote_views 
-POST api/views/remove_project 
- GET api/views/show 
-POST api/views/update 
- GET api/webservices/list 
- GET api/webservices/response_example 
+POST api/views/create
+ GET api/views/define
+POST api/views/delete
+ GET api/views/list
+ GET api/views/local_views
+POST api/views/manual_measure
+POST api/views/mode
+POST api/views/move
+ GET api/views/move_options
+ GET api/views/projects
+POST api/views/regexp
+ GET api/views/remote_views
+POST api/views/remove_project
+ GET api/views/show
+POST api/views/update
+ GET api/webservices/list
+ GET api/webservices/response_example
 
 =head1 DIAGNOSTICS
 
